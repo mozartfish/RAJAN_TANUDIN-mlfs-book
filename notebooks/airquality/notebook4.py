c0f41d542c99d6dcfad4e9ed2fc578bb6b76e264
Pranav Rajan <rajan.pranav320@gmail.com>
2025-11-18 19:13:41 +0100

    add stuff

---
+++ notebooks/airquality/notebook4.py
@@ -0,0 +1,241 @@
+# %% [markdown]
+# # <span style="font-width:bold; font-size: 3rem; color:#1EB182;"> **Air Quality** </span><span style="font-width:bold; font-size: 3rem; color:#333;">- Part 04: Batch Inference</span>
+# 
+# ## 🗒️ This notebook is divided into the following sections:
+# 
+# 1. Download model and batch inference data
+# 2. Make predictions, generate PNG for forecast
+# 3. Store predictions in a monitoring feature group adn generate PNG for hindcast
+# 
+
+# %% [markdown]
+# ## <span style='color:#ff5f27'> 📝 Imports
+# 
+
+# %%
+import sys
+from pathlib import Path
+import os
+
+
+def is_google_colab() -> bool:
+    if "google.colab" in str(get_ipython()):
+        return True
+    return False
+
+
+def clone_repository() -> None:
+    !git clone https://github.com/featurestorebook/mlfs-book.git
+    %cd mlfs-book
+
+
+def install_dependencies() -> None:
+    !pip install --upgrade uv
+    !uv pip install --all-extras --system --requirement pyproject.toml
+
+
+if is_google_colab():
+    clone_repository()
+    install_dependencies()
+    root_dir = str(Path().absolute())
+    print("Google Colab environment")
+else:
+    root_dir = Path().absolute()
+    # Strip ~/notebooks/ccfraud from PYTHON_PATH if notebook started in one of these subdirectories
+    if root_dir.parts[-1:] == ('airquality',):
+        root_dir = Path(*root_dir.parts[:-1])
+    if root_dir.parts[-1:] == ('notebooks',):
+        root_dir = Path(*root_dir.parts[:-1])
+    root_dir = str(root_dir)
+    print("Local environment")
+
+# Add the root directory to the `PYTHONPATH` to use the `recsys` Python module from the notebook.
+if root_dir not in sys.path:
+    sys.path.append(root_dir)
+print(f"Added the following directory to the PYTHONPATH: {root_dir}")
+
+# Read the API keys and configuration variables from the file <root_dir>/.env
+from mlfs import config
+
+if os.path.exists(f"{root_dir}/.env"):
+    settings = config.HopsworksSettings(_env_file=f"{root_dir}/.env")
+
+# %%
+import datetime
+import pandas as pd
+from xgboost import XGBRegressor
+import hopsworks
+import json
+from mlfs.airquality import util
+
+# %%
+today = datetime.datetime.now() - datetime.timedelta(0)
+tomorrow = today + datetime.timedelta(days=1)
+today
+
+# %% [markdown]
+# ## <span style="color:#ff5f27;"> 📡 Connect to Hopsworks Feature Store </span>
+# 
+
+# %%
+project = hopsworks.login()
+fs = project.get_feature_store()
+
+secrets = hopsworks.get_secrets_api()
+location_str = secrets.get_secret("SENSOR_LOCATION_JSON").value
+location = json.loads(location_str)
+country = location['country']
+city = location['city']
+street = location['street']
+
+# %% [markdown]
+# ## <span style="color:#ff5f27;">🪝 Download the model from Model Registry</span>
+# 
+
+# %%
+mr = project.get_model_registry()
+
+retrieved_model = mr.get_model(
+    name="air_quality_xgboost_model",
+    version=1,
+)
+
+fv = retrieved_model.get_feature_view()
+
+# Download the saved model artifacts to a local directory
+saved_model_dir = retrieved_model.download()
+
+# %%
+# Loading the XGBoost regressor model and label encoder from the saved model directory
+# retrieved_xgboost_model = joblib.load(saved_model_dir + "/xgboost_regressor.pkl")
+retrieved_xgboost_model = XGBRegressor()
+
+retrieved_xgboost_model.load_model(saved_model_dir + "/model.json")
+
+# Displaying the retrieved XGBoost regressor model
+retrieved_xgboost_model
+
+# %% [markdown]
+# ## <span style="color:#ff5f27;">✨ Get Weather Forecast Features with Feature View </span>
+# 
+
+# %%
+weather_fg = fs.get_feature_group(
+    name='weather',
+    version=1,
+)
+batch_data = weather_fg.filter(weather_fg.date >= today).read()
+batch_data
+
+# %% [markdown]
+# ### <span style="color:#ff5f27;">🤖 Making the predictions</span>
+# 
+
+# %%
+batch_data['predicted_pm25'] = retrieved_xgboost_model.predict(
+    batch_data[['temperature_2m_mean', 'precipitation_sum', 'wind_speed_10m_max', 'wind_direction_10m_dominant']]
+)
+batch_data
+
+# %%
+batch_data.info()
+
+# %% [markdown]
+# ### <span style="color:#ff5f27;">🤖 Saving the predictions (for monitoring) to a Feature Group</span>
+# 
+
+# %%
+batch_data['street'] = street
+batch_data['city'] = city
+batch_data['country'] = country
+# Fill in the number of days before the date on which you made the forecast (base_date)
+batch_data['days_before_forecast_day'] = range(1, len(batch_data) + 1)
+batch_data = batch_data.sort_values(by=['date'])
+batch_data
+
+# %%
+batch_data.info()
+
+# %% [markdown]
+# ### Create Forecast Graph
+# 
+# Draw a graph of the predictions with dates as a PNG and save it to the github repo
+# Show it on github pages
+# 
+
+# %%
+pred_file_path = f"{root_dir}/docs/air-quality/assets/img/pm25_forecast.png"
+plt = util.plot_air_quality_forecast(city, street, batch_data, pred_file_path)
+
+plt.show()
+
+# %%
+# Get or create feature group
+monitor_fg = fs.get_or_create_feature_group(
+    name='aq_predictions',
+    description='Air Quality prediction monitoring',
+    version=1,
+    primary_key=['city', 'street', 'date', 'days_before_forecast_day'],
+    event_time="date",
+)
+
+# %%
+monitor_fg.insert(batch_data, wait=True)
+
+# %%
+# We will create a hindcast chart for  only the forecasts made 1 day beforehand
+monitoring_df = monitor_fg.filter(monitor_fg.days_before_forecast_day == 1).read()
+monitoring_df
+
+# %%
+air_quality_fg = fs.get_feature_group(name='air_quality', version=1)
+air_quality_df = air_quality_fg.read()
+air_quality_df
+
+# %%
+outcome_df = air_quality_df[['date', 'pm25']]
+preds_df = monitoring_df[['date', 'predicted_pm25']]
+
+hindcast_df = pd.merge(preds_df, outcome_df, on="date")
+hindcast_df = hindcast_df.sort_values(by=['date'])
+
+# If there are no outcomes for predictions yet, generate some predictions/outcomes from existing data
+if len(hindcast_df) == 0:
+    hindcast_df = util.backfill_predictions_for_monitoring(
+        weather_fg, air_quality_df, monitor_fg, retrieved_xgboost_model
+    )
+hindcast_df
+
+# %% [markdown]
+# ### Plot the Hindcast comparing predicted with forecasted values (1-day prior forecast)
+# 
+# **This graph will be empty to begin with - this is normal.**
+# 
+# After a few days of predictions and observations, you will get data points in this graph.
+# 
+
+# %%
+hindcast_file_path = f"{root_dir}/docs/air-quality/assets/img/pm25_hindcast_1day.png"
+plt = util.plot_air_quality_forecast(city, street, hindcast_df, hindcast_file_path, hindcast=True)
+plt.show()
+
+# %% [markdown]
+# ### Upload the prediction and hindcast dashboards (png files) to Hopsworks
+# 
+
+# %%
+dataset_api = project.get_dataset_api()
+str_today = today.strftime("%Y-%m-%d")
+if dataset_api.exists("Resources/airquality") == False:
+    dataset_api.mkdir("Resources/airquality")
+dataset_api.upload(pred_file_path, f"Resources/airquality/{city}_{street}_{str_today}", overwrite=True)
+dataset_api.upload(hindcast_file_path, f"Resources/airquality/{city}_{street}_{str_today}", overwrite=True)
+
+proj_url = project.get_url()
+print(f"See images in Hopsworks here: {proj_url}/settings/fb/path/Resources/airquality")
+
+# %% [markdown]
+# ---
+# 
+
+
